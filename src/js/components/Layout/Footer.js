7dd0f40b3b65516638b917bccac8642e858b8ded
Lee Raulin <leeraulin@gmail.com>
2018-02-26 11:35:04 -0500

    updated resume

--- src/js/components/Layout/Footer.js
+++ src/js/components/Layout/Footer.js
@@ -25,6 +25,7 @@ export default class Footer extends React.Component {
           </div>
         </div>
         <div class="col-md-4"></div>
+        // TODO: Fix styling
         <div class="col-md-4">
           <h3>Social Media</h3>
           <ul>
--- src/js/pages/Resume.js
+++ src/js/pages/Resume.js
@@ -12,10 +12,17 @@ export default class Resume extends Component {
         <h2>Front End</h2>
         <p>HTML, CSS, Bootstrap</p>
         <h2>Back End</h2>
-
+        <p>MongoDB, MS SQL Server, Firebase, NodeJS</p>
+        <h2>Soft Skills/Attributes</h2>
+        <ul>
+          <li>Exceptional verbal comprehension: reading, writing, etc.</li>
+          <li>Polite, agreeable, and very resistant to stress.</li>
+          <li>Curiosity, love of learning, interest in abstract concepts.</li>
+          <li>Critical thinking/objectivity/open-mindedness.</li>
+        </ul>
         <h2>Education</h2>
         <h3><a href="https://www.ncu.edu/">Northcentral University</a></h3>
-        <p>Master of Arts in Psychology</p>
+        <p>Master of Arts in General Psychology</p>
         <ul>
           <li>
             Composed a literature review on &nbsp;
@@ -26,8 +33,14 @@ export default class Resume extends Component {
           <li>Completed while working full-time, demonstrating ability to
             manage time and learn independantly.</li>
         </ul>
-        <h3>Free Code Camp (in progress)</h3>
-        <p>Front Development Certification</p>
+        <h3><a href="http://www.smcm.edu/">St. Mary's College</a></h3>
+        <p>Batchelor of Arts in Psychology</p>
+        <h3><a href="https://www.freecodecamp.org/">Free Code Camp
+          </a>&nbsp;(in progress)</h3>
+        <p>
+          <a href="https://www.freecodecamp.org/lraulin/front-end-certification">
+          Front End Development Certification</a>
+        </p>
         <ul>
           <li>Created 11 front-end projects and 4 React projects.</li>
           <li>Completed 47 algorithm scripting challenges.</li>
@@ -39,6 +52,13 @@ export default class Resume extends Component {
           <li>n-tier architecture using webforms for interface, C# for business
             layer, and MS Sequel Server as backend.</li>
           <li>User state management.</li>
+        </ul>
+        <h2>Military Experience</h2>
+        <h3>USCG, Boatswain's Mate</h3>
+        <ul>
+          <li>District 7 Headquarter's Aids to Navigation Discrepancy Manager</li>
+          <li>Attained E5 in less than 3 years.</li>
+          <li>Honorable discharge.</li>
         </ul>
       </div>
     )
